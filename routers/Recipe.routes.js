54776aec7d7c37235a59005aeb80fff4f41538f3
yasg-uru <yashpawar12122004@gmail.com>
2024-06-27 14:45:16 +0530

    modified recipe router

--- routers/Recipe.routes.js
+++ routers/Recipe.routes.js
@@ -1,16 +1,32 @@
-import {Router} from "express";
-import { Comparerecipewithdailygoals, GetAdjustedRecipe, GetRecipesAccordingtoMissingIngredients, GetRecipesBySearch, GetRecipesBypagination, compareRecipeBynutrients, createRecipe, deleterecipe, updaterecipe } from "../controllers/Recipe.controller.js";
-import {isAuthenticated} from "../middlewares/auth.middleware.js";
+import { Router } from "express";
+import {
+  Comparerecipewithdailygoals,
+  GetAdjustedRecipe,
+  GetRecipesAccordingtoMissingIngredients,
+  GetRecipesBySearch,
+  GetRecipesBypagination,
+  LikeRecipebyuser,
+  compareRecipeBynutrients,
+  createRecipe,
+  deleterecipe,
+  updaterecipe,
+} from "../controllers/Recipe.controller.js";
+import { isAuthenticated } from "../middlewares/auth.middleware.js";
 
-const RecipeRouter=Router();
-RecipeRouter.post("/create",isAuthenticated,createRecipe);
-RecipeRouter.put("/update/:id",isAuthenticated,updaterecipe);
-RecipeRouter.delete("/delete/:id",isAuthenticated,deleterecipe);
-RecipeRouter.get("/bysearch",isAuthenticated,GetRecipesBySearch);
-RecipeRouter.get("/bypagination",isAuthenticated,GetRecipesBypagination);
-RecipeRouter.get("/compare",isAuthenticated,Comparerecipewithdailygoals);
-RecipeRouter.get("/compareRecipes",isAuthenticated,compareRecipeBynutrients);
-RecipeRouter.get("/bymissing",isAuthenticated,GetRecipesAccordingtoMissingIngredients);
-RecipeRouter.get("/adjusted",isAuthenticated,GetAdjustedRecipe);
+const RecipeRouter = Router();
+RecipeRouter.post("/create", isAuthenticated, createRecipe);
+RecipeRouter.put("/update/:id", isAuthenticated, updaterecipe);
+RecipeRouter.delete("/delete/:id", isAuthenticated, deleterecipe);
+RecipeRouter.get("/bysearch", isAuthenticated, GetRecipesBySearch);
+RecipeRouter.get("/bypagination", isAuthenticated, GetRecipesBypagination);
+RecipeRouter.get("/compare", isAuthenticated, Comparerecipewithdailygoals);
+RecipeRouter.get("/compareRecipes", isAuthenticated, compareRecipeBynutrients);
+RecipeRouter.get(
+  "/bymissing",
+  isAuthenticated,
+  GetRecipesAccordingtoMissingIngredients
+);
+RecipeRouter.get("/adjusted", isAuthenticated, GetAdjustedRecipe);
+RecipeRouter.put("/like/:recipeId", isAuthenticated, LikeRecipebyuser);
 
 export default RecipeRouter;
