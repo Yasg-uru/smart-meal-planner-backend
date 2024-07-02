81e87044552fda16b45ef477d3d152a750557f51
yasg-uru <yashpawar12122004@gmail.com>
2024-07-02 18:16:22 +0530

    correccted

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -3,19 +3,21 @@ import User from "../models/user.models.js";
 import Errorhandler from "../utils/Errorhandler.utils.js";
 import SendToken from "../utils/SendToken.utils.js";
 export const RegisterUser = catchaysynerror(async (req, res, next) => {
-  try {
-    const { Name, Email } = req.body;
-    const exisitinguser = await User.findOne({ $or: [{ Name }, { Email }] });
-    if (exisitinguser) {
-      return next(
-        new Errorhandler(404, "Already user Exist with this email or username")
-      );
-    }
-    const newuser = await User.create(req.body);
-    SendToken(newuser, res, 200);
-  } catch (error) {
-    return next(new Errorhandler(500, "Internal server error "));
+  // try {
+  const { Name, Email } = req.body;
+  const exisitinguser = await User.findOne({ $or: [{ Name }, { Email }] });
+  console.log("this is existing user:", exisitinguser);
+  if (exisitinguser) {
+    return next(
+      new Errorhandler(404, "Already user Exist with this email or username")
+    );
   }
+
+  const newuser = await User.create(req.body);
+  SendToken(newuser, res, 200);
+  // } catch (error) {
+  //   return next(new Errorhandler(500, "Internal server error "));
+  // }
 });
 export const Login = catchaysynerror(async (req, res, next) => {
   try {
